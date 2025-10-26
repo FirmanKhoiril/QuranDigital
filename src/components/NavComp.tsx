5469902fe0d5a61dfbff36d33d8c0de5bf517967
FirmanKhoiril <firmankhoiril13@gmail.com>
2025-10-26 13:51:00 +0700

    make more hidden the bg of the logo

--- src/components/NavComp.tsx
+++ src/components/NavComp.tsx
@@ -7,7 +7,7 @@ const NavComp = () => {
   return (
     <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
       <Link to="/">
-        <Typography variant="h4" sx={{ letterSpacing: "0.10rem", display: "flex", gap: 1.5, alignItems: "center", backgroundColor: "rgba(238, 145, 61, 0.1)", padding: 1, borderRadius: 2  }}>
+        <Typography variant="h4" sx={{ letterSpacing: "0.10rem", display: "flex", gap: 1.5, alignItems: "center", backgroundColor: "rgba(238, 145, 61, 0.04)", padding: 1, borderRadius: 2  }}>
           <IoBookOutline color="#ee913d" />
           <span className="logo ">Hidayah Qur’an</span>
         </Typography>
