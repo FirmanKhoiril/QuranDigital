c44edd727b93c1c548453e4363780ec43915f46f
FirmanKhoiril <firmankhoiril13@gmail.com>
2025-10-26 14:13:28 +0700

    change footer domain website from quranhub to hidayah-quran

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Box sx={{ minHeight: 200, display: "flex", justifyContent: "space-around", flexDirection: { xs: "column", md: "row-reverse", overflow: "hidden" }, marginTop: 10 }}>
       <Typography>
-        <span className="font-quicksand">©2025 QuranHub.vercel.app All rights reserved.</span>
+        <span className="font-quicksand">©2025 hidayah-quran.vercel.app All rights reserved.</span>
       </Typography>
       <Typography>
         <span className=" text-white/50 flex items-center gap-4 cursor-default text-[13px] font-quicksand">
