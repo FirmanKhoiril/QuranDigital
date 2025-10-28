2d1e2ce523fd384e10ba025832c7f7a2618de471
Firman Khoiril Rohmatullah <96419393+FirmanKhoiril@users.noreply.github.com>
2025-10-28 07:20:24 +0700

    Update Footer.tsx

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { HeartIcon } from "lucide-react";
 
 const Footer = () => {
   return (
-    <Box sx={{ minHeight: 200, display: "flex", justifyContent: "space-around", flexDirection: { xs: "column", md: "row-reverse", overflow: "hidden" }, marginTop: 10 }}>
+    <Box sx={{ minHeight: 200, borderTop: "1px solid rgba(255, 255, 255, 0.2)", display: "flex", justifyContent: "space-around", flexDirection: { xs: "column", md: "row-reverse", overflow: "hidden" }, marginTop: 10 }}>
       <Typography>
         <span className="font-quicksand">©2025 hidayah-quran.vercel.app All rights reserved.</span>
       </Typography>
